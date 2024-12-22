659fcbfed837f18dcad592984218414d6b7e7e11
Markus Wendorf <markuswendorf@web.de>
2024-12-22 22:28:46 +0100

    re-order

--- src/main.rs
+++ src/main.rs
@@ -8,60 +8,57 @@ use std::net::SocketAddr;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 
-use axum::extract::{ConnectInfo, Path, State};
-use axum::http::{HeaderMap, StatusCode};
-use axum::response::{IntoResponse, Redirect};
-use axum::routing::{get, post};
-use axum::Json;
-use axum::Router;
+use axum::{
+    extract::{ConnectInfo, Path, State},
+    http::{HeaderMap, StatusCode},
+    response::{IntoResponse, Redirect},
+    routing::{get, post},
+    Json, Router,
+};
 use flume::Sender;
-use metrics::{Metric, MetricsStorage};
 use rusqlite::Error::SqliteFailure;
 
 use id::generate_id;
-
+use metrics::{Metric, MetricsStorage};
 use sqlite::SqliteStorage;
 use structs::{CreateShortUrl, ShortUrlCreated};
 
 struct AppState {
-    pub storage: SqliteStorage,
-    pub sender: Sender<Metric>,
+    storage: SqliteStorage,
+    sender: Sender<Metric>,
 }
 
 #[tokio::main]
 async fn main() {
     let storage = SqliteStorage::new();
-    let mut metrics = MetricsStorage::new();
+    let metrics = MetricsStorage::new();
 
-    println!("Key count: {:?}", metrics.key_count());
+    let (sender, receiver) = flume::unbounded();
+    tokio::spawn(metrics_handler(receiver, metrics));
 
-    let (sender, receiver) = flume::unbounded::<Metric>();
-    tokio::spawn(async move {
-        while let Ok(metric) = receiver.recv_async().await {
-            let _ = metrics.add(metric);
-        }
-    });
-
-    let shared_state = Arc::new(Mutex::new(AppState { storage, sender }));
-
-    let app = Router::new()
-        .route("/", get(health_check))
-        .route("/create-short-url", post(create_short_url))
-        .route("/:id", get(redirect_to_url))
-        .with_state(shared_state);
-
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3333").await.unwrap();
+    let state = Arc::new(Mutex::new(AppState { storage, sender }));
+    let app = create_router(state);
 
     axum::serve(
-        listener,
+        tokio::net::TcpListener::bind("0.0.0.0:3333").await.unwrap(),
         app.into_make_service_with_connect_info::<SocketAddr>(),
     )
     .await
     .unwrap();
 }
 
-async fn health_check() -> impl IntoResponse {
-    StatusCode::OK.into_response()
+fn create_router(state: Arc<Mutex<AppState>>) -> Router {
+    Router::new()
+        .route("/", get(|| async { StatusCode::OK }))
+        .route("/create-short-url", post(create_short_url))
+        .route("/:id", get(redirect_to_url))
+        .with_state(state)
+}
+
+async fn metrics_handler(receiver: flume::Receiver<Metric>, mut metrics: MetricsStorage) {
+    while let Ok(metric) = receiver.recv_async().await {
+        metrics.add(metric).ok();
+    }
 }
 
 async fn create_short_url(
@@ -69,34 +66,15 @@ async fn create_short_url(
     Json(payload): Json<CreateShortUrl>,
 ) -> impl IntoResponse {
     let app = state.lock().await;
-
-    let mut selected_id: Option<String> = None;
-
-    let max_retries = 5;
     let mut retries = 0;
 
-    loop {
-        let generated_id = generate_id();
-
-        match app.storage.set(&generated_id, &payload.url) {
-            // Duplicate key, try again
-            Err(SqliteFailure(err, _)) if err.extended_code == 1555 && retries < max_retries => {
-                retries += 1;
-                continue;
-            }
-            Err(err) => {
-                println!("Error {:?}", err);
-                break;
-            }
-            Ok(_) => {
-                selected_id = Some(generated_id);
-                break;
-            }
+    while retries < 5 {
+        let id = generate_id();
+        match app.storage.set(&id, &payload.url) {
+            Ok(_) => return (StatusCode::CREATED, Json(ShortUrlCreated { id })).into_response(),
+            Err(SqliteFailure(err, _)) if err.extended_code == 1555 => retries += 1,
+            Err(_) => break,
         }
-    }
-
-    if let Some(id) = selected_id {
-        return (StatusCode::CREATED, Json(ShortUrlCreated { id })).into_response();
     }
 
     StatusCode::INTERNAL_SERVER_ERROR.into_response()
@@ -111,26 +89,25 @@ async fn redirect_to_url(
     let app = state.lock().await;
 
     if let Some(url) = app.storage.get(&id) {
-        let _ = app
-            .sender
-            .send_async(Metric {
-                ip: header_to_string(&headers, "cloudfront-viewer-address")
-                    .unwrap_or_else(|| addr.ip().to_string()),
-                url: url.clone(),
-                shorthand_id: id,
-                android: header_to_bool(&headers, "cloudfront-is-android-viewer"),
-                ios: header_to_bool(&headers, "cloudfront-is-ios-viewer"),
-                mobile: header_to_bool(&headers, "cloudfront-is-mobile-viewer"),
-                region_name: header_to_string(&headers, "cloudfront-viewer-country-region-name"),
-                country: header_to_string(&headers, "cloudfront-viewer-country"),
-                city: header_to_string(&headers, "cloudfront-viewer-city"),
-                zip_code: header_to_string(&headers, "cloudfront-viewer-postal-code"),
-                time_zone: header_to_string(&headers, "cloudfront-viewer-time-zone"),
-                user_agent: header_to_string(&headers, "user-agent"),
-                longitude: header_to_float(&headers, "cloudfront-viewer-longitude"),
-                latitude: header_to_float(&headers, "cloudfront-viewer-latitude"),
-            })
-            .await;
+        let metric = Metric {
+            ip: header_to_string(&headers, "cloudfront-viewer-address")
+                .unwrap_or_else(|| addr.ip().to_string()),
+            url: url.clone(),
+            shorthand_id: id,
+            android: header_to_bool(&headers, "cloudfront-is-android-viewer"),
+            ios: header_to_bool(&headers, "cloudfront-is-ios-viewer"),
+            mobile: header_to_bool(&headers, "cloudfront-is-mobile-viewer"),
+            region_name: header_to_string(&headers, "cloudfront-viewer-country-region-name"),
+            country: header_to_string(&headers, "cloudfront-viewer-country"),
+            city: header_to_string(&headers, "cloudfront-viewer-city"),
+            zip_code: header_to_string(&headers, "cloudfront-viewer-postal-code"),
+            time_zone: header_to_string(&headers, "cloudfront-viewer-time-zone"),
+            user_agent: header_to_string(&headers, "user-agent"),
+            longitude: header_to_float(&headers, "cloudfront-viewer-longitude"),
+            latitude: header_to_float(&headers, "cloudfront-viewer-latitude"),
+        };
+
+        app.sender.send_async(metric).await.ok();
 
         return Redirect::temporary(&url).into_response();
     }
@@ -145,13 +122,9 @@ fn header_to_bool(headers: &HeaderMap, key: &str) -> Option<bool> {
 fn header_to_string(headers: &HeaderMap, key: &str) -> Option<String> {
     headers
         .get(key)
-        .map(|value| String::from(value.to_str().unwrap_or_default()))
+        .and_then(|v| v.to_str().ok().map(String::from))
 }
 
 fn header_to_float(headers: &HeaderMap, key: &str) -> Option<f64> {
-    if let Some(value) = headers.get(key) {
-        return value.to_str().unwrap_or_default().parse::<f64>().ok();
-    }
-
-    None
+    headers.get(key).and_then(|v| v.to_str().ok()?.parse().ok())
 }
